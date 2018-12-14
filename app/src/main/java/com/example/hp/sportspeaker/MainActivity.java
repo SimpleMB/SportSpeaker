744ff3b7f9c139d010ddcef89e0b10733a374713
SimpleMB <ab.mazur@gmail.com>
2018-12-14 17:30:21 +0100

    Commit no2.
    1. Changed media player to SoundPool. WORKS PERFECTLY. Awesome :)

--- app/src/main/java/com/example/hp/sportspeaker/MainActivity.java
+++ app/src/main/java/com/example/hp/sportspeaker/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.hp.sportspeaker;
 
-import android.media.MediaPlayer;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,14 +9,23 @@ import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private MediaPlayer mediaPlayer;
+    private SoundPool sp;
+    private int sound1, sound2, sound3, sound4, sound5, sound6;
     private ImageButton imgBoxing, imgKickboxing, imgJudo, imgKarate, imgAikido, imgTaekwondo;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        sp = new SoundPool(2, AudioManager.STREAM_MUSIC, 0);
+        sound1 = sp.load(getApplicationContext(),R.raw.boxing ,1);
+        sound2 = sp.load(getApplicationContext(),R.raw.kickboxing ,1);
+        sound3 = sp.load(getApplicationContext(),R.raw.judo ,1);
+        sound4 = sp.load(getApplicationContext(),R.raw.karate ,1);
+        sound5 = sp.load(getApplicationContext(),R.raw.aikido ,1);
+        sound6 = sp.load(getApplicationContext(),R.raw.taekwondo ,1);
+
 
         imgBoxing = findViewById(R.id.imgBoxing);
         imgKickboxing = findViewById(R.id.imgKickboxing);
@@ -31,58 +41,38 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         imgKarate.setOnClickListener(MainActivity.this);
         imgAikido.setOnClickListener(MainActivity.this);
         imgTaekwondo.setOnClickListener(MainActivity.this);
-
-
     }
 
     @Override
     public void onClick(View btnIsClicked) {
-        String imgTagSound = btnIsClicked.getTag().toString();
 
         switch (btnIsClicked.getId()){
 
             case R.id.imgBoxing:
-
-                playSound(imgTagSound);
-
+                sp.play(sound1, 1.0f, 1.0f, 0,0,1f);
                 break;
 
             case R.id.imgKickboxing:
-
-                playSound(imgTagSound);
-
+                sp.play(sound2, 1.0f, 1.0f, 0,0,1f);
                 break;
 
             case R.id.imgJudo:
-
-                playSound(imgTagSound);
-
+                sp.play(sound3, 1.0f, 1.0f, 0,0,1f);
                 break;
 
             case R.id.imgKarate:
-
-                playSound(imgTagSound);
-
+                sp.play(sound4, 1.0f, 1.0f, 0,0,1f);
                 break;
 
             case R.id.imgAikido:
-
-                playSound(imgTagSound);
-
+                sp.play(sound5, 1.0f, 1.0f, 0,0,1f);
                 break;
 
             case R.id.imgTeakwondo:
-
-                playSound(imgTagSound);
-
+                sp.play(sound6, 1.0f, 1.0f, 0,0,1f);
                 break;
         }
 
     }
-    private void playSound (String imgTagSound) {
 
-        mediaPlayer = MediaPlayer.create(this, getResources().getIdentifier(imgTagSound, "raw", getPackageName()));
-        mediaPlayer.start();
-
-    }
 }
